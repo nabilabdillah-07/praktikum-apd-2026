b1a691a6b1b3b9d3b6063bdda40aa09812add9ec
nabilabdillah-07 <nabil.abdillah.me@gmail.com>
2026-09-16 16:16:16 +0800

    ngetest isi modul

--- pertemuan1.py
+++ pertemuan1.py
@@ -1,1 +1,81 @@
-print("ini mo nyoba push lagi") 
+#numerik
+
+angka1 = 5
+angka2 = 7
+
+hasil = angka1 + angka2
+print(hasil)
+
+#float
+a = 3.45
+b = 1.23
+hitung = a + b
+print(hitung)
+
+#string
+nama = "nabil"
+hobi = "ngoding"
+
+print(nama)
+print(hobi)
+
+print('''halo saya nbil suka ngoding''')
+
+print("aku""rajin""ngoding")
+print("aku"+"rajin")
+
+teks = "Rahmat Flowchart"
+print(teks[3])
+print(teks[3:6])
+print(teks[7:])
+print(teks[:6])
+print(teks[1:4:2])
+print(teks[:-1])
+
+biodata = ["nbil", 19,"2026", 70.5, True, ["APD","26"] ]
+print(biodata)
+
+mata_kuliah = ["APD","Logika Matematika","Kalkulus"]
+print(mata_kuliah[0])
+print(mata_kuliah[1])
+print(mata_kuliah[2])
+print(mata_kuliah[0:1])
+print(mata_kuliah[:2])
+print(mata_kuliah[1:])
+print(mata_kuliah[::2])
+print(mata_kuliah[:-1])
+
+buku = {
+'judul' : 'Atomic Habits',
+'penulis' : 'James Clear',
+'halaman' : 320
+}
+...
+# Mengakses Value dalam Dictionary
+print(buku['judul'])
+print(buku['penulis'])
+print(buku['halaman'])
+
+angka = int("40")
+print(angka)
+
+ubah = float("99.99")
+print(ubah)
+
+name = input("masukin nama :")
+print(name)
+
+gaji = int(input("masukkin gaji :"))
+print(gaji)
+
+print (2 < 4)
+print (5 > 5)
+print(2 < 4 and 5 > 10)
+
+zidan = {
+'pagi' : 'Kalkulus',
+'siang' : 'Algoritma Pemrograman Dasar',
+'sore' : 'Bahasa Inggris',
+}
+
+print(zidan['siang'])
